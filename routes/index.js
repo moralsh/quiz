8735149c77496aba6a469781f42078ee5ecd2d55
Raúl Morales Hidalgo <rmorales@innova.uned.es>
2015-06-29 21:38:38 +0200

    P2P Módulo 6 parte 2 (página de créditos)

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* GET Authors page. */
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Autores' });
+});
+
 router.get('/quizzes/question', quizController.question);
 router.get('/quizzes/answer', quizController.answer);
 

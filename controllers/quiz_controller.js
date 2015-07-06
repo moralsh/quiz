5fd40390aabd8b1b1de8b744aa8e130c1564e841
Raul Morales <rmorales@innova.uned.es>
2015-07-06 13:24:27 +0200

    creación del modelo y la BDD

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,19 @@
+var models = require('../models/models.js');
+
 // GET quizzes/question
 exports.question = function( req, res) {
-    res.render('quizzes/question', { pregunta: 'Capital de Italia'});
+    models.Quiz.findAll().success(function(quiz) {
+        res.render('quizes/question', { pregunta: quiz[0].pregunta});
+    })
 }
 
 // GET quizzes/answer
 exports.answer = function( req, res) {
-    if (req.query.respuesta === 'Roma') {
-        res.render('quizzes/answer', { respuesta: 'Correcto'});
-    } else {
-        res.render('quizzes/answer', { respuesta: 'Incorrecto'});
-    }
+    models.Quiz.findAll().success(function(quiz) {
+        if (req.query.respuesta === quiz[0].respuesta) {
+            res.render('quizes/answer', { respuesta: 'Correcto' });
+        } else {
+            res.render('quizes/answer', { respuesta: 'Incorrecto'});
+        }
+    })
 };

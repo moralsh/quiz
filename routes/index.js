72a0addd04845c1a5e1abd879918d198857cedf2
Raúl Morales Hidalgo <moralsh@gmail.com>
2015-07-16 13:27:59 +0200

    Añadimos la posibilidad de añadir preguntas

--- routes/index.js
+++ routes/index.js
@@ -19,5 +19,6 @@ router.param('quizId', quizController.load);  // autoload :quizId
 router.get('/quizzes',                      quizController.index);
 router.get('/quizzes/:quizId(\\d+)',        quizController.show);
 router.get('/quizzes/:quizId(\\d+)/answer', quizController.answer);
-
+router.get('/quizzes/new',                  quizController.new);
+router.post('/quizzes/create',              quizController.create);
 module.exports = router;
